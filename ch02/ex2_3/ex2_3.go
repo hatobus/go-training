6c2dcbd6587bc93e37c7754ce4d9a447cf48079f
hatobus <s1240056@gmail.com>
2020-09-04 00:34:01 +0900

    [ex2-3] ループを使ったPopCountのテストとベンチマーク

---
+++ ch02/ex2_3/ex2_3.go
@@ -0,0 +1,19 @@
+package main
+
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func PopCount(x uint64) int {
+	var cnt int
+
+	for i := 0; i < 8; i++ {
+		cnt += int(pc[byte(x>>(uint(i)*8))])
+	}
+
+	return cnt
+}
