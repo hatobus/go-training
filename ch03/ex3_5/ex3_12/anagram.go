5877b87aa3f25e5885186de7b01412fa59519549
hatobus <s1240056@gmail.com>
2020-09-29 22:08:46 +0900

    アナグラム

--- ch03/ex3_5/ex3_12/anagram.go
+++ ch03/ex3_5/ex3_12/anagram.go
@@ -1,1 +1,37 @@
-package ex3_12
+package anagram
+
+import (
+	"sort"
+	"strings"
+)
+
+func Anagram(s1, s2 string) bool {
+	s1 = deleteSpace(strings.ToLower(s1))
+	s2 = deleteSpace(strings.ToLower(s2))
+
+	if len(s1) != len(s2) {
+		return false
+	}
+
+	runeS1 := []rune(s1)
+	runeS2 := []rune(s2)
+
+	sort.Slice(runeS1, func(i, j int) bool {
+		return runeS1[i] < runeS1[j]
+	})
+
+	sort.Slice(runeS2, func(i, j int) bool {
+		return runeS2[i] < runeS2[j]
+	})
+
+	if string(runeS1) == string(runeS2) {
+		return true
+	}
+
+	return false
+}
+
+func deleteSpace(s string) string {
+	elems := strings.Split(s, " ")
+	return strings.Join(elems, "")
+}
--- ch03/ex3_5/ex3_12/anagram_test.go
+++ ch03/ex3_5/ex3_12/anagram_test.go
@@ -1,1 +1,58 @@
 package anagram
+
+import "testing"
+
+func TestAnagram(t *testing.T) {
+	type testData struct {
+		s1   string
+		s2   string
+		same bool
+	}
+
+	testCases := map[string]testData{
+		"英語でアナグラムを検知できる": {
+			s1:   "abc",
+			s2:   "cba",
+			same: true,
+		},
+		"英語でアナグラムでない時": {
+			s1:   "abc",
+			s2:   "aab",
+			same: false,
+		},
+		"長い英文(完全パングラム)": {
+			s1:   "Jumbling vext frowzy hacks PDQ",
+			s2:   "Glum Schwartzkopf vexd by NJ IQ",
+			same: true,
+		},
+		"日本語でアナグラム": {
+			s1:   "こんにちは",
+			s2:   "こんちには",
+			same: true,
+		},
+		"日本語でアナグラムでない時": {
+			s1:   "こんにちは",
+			s2:   "こんばんは",
+			same: false,
+		},
+		"日本語でアナグラム(サロゲートペア)": {
+			s1:   "𠮷野家で𩸽",
+			s2:   "𩸽で𠮷野家",
+			same: true,
+		},
+	}
+
+	for testName, tc := range testCases {
+		tc := tc
+		t.Run(testName, func(t *testing.T) {
+			t.Parallel()
+
+			isSame := Anagram(tc.s1, tc.s2)
+
+			if isSame != tc.same {
+				t.Logf("invalid testcase, s1: %v, s2: %v", tc.s1, tc.s2)
+				t.Fatalf("invalid output, want %v but got %v", tc.same, isSame)
+			}
+		})
+	}
+}
