370ca36f734f565a9ea5119c578c0894f10641db
hatobus <s1240056@gmail.com>
2020-10-01 09:59:30 +0900

    editするissueを取得するところまで

--- ch04/ex4_5/ex4.11/command/edit.go
+++ ch04/ex4_5/ex4.11/command/edit.go
@@ -1,1 +1,52 @@
 package command
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/hatobus/go-training/ch04/ex4_5/ex4.11/github"
+
+	"github.com/google/uuid"
+)
+
+var uu uuid.UUID
+
+func init() {
+	var err error
+	uu, err = uuid.NewRandom()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func EditIssue(owner, repo, number string) error {
+	editor := os.Getenv("ISSUE_EDITOR")
+	if editor == "" {
+		// vi ならどこでも入っているはずなのでデフォルトは vi にする
+		editor = "vi"
+	}
+
+	tmpFile, err := ioutil.TempFile("", fmt.Sprintf("editor_issue_%v", uu.String()))
+	if err != nil {
+		return err
+	}
+	defer func() {
+		err = tmpFile.Close()
+		if err != nil {
+			log.Println(err)
+		}
+		err = os.Remove(tmpFile.Name())
+		if err != nil {
+			log.Println(err)
+		}
+	}()
+
+	issue, err := github.ReadIssueFromIdentifier(owner, repo, number)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/google/go-cmp v0.5.2
+	github.com/google/uuid v1.1.2 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
 	gopl.io v0.0.0-20200323155855-65c318dde95e
 )
