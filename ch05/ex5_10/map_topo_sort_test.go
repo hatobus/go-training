fe3da01968154c7e6d10ee8f294304de58b0f843
hatobus <s1240056@gmail.com>
2020-10-23 00:56:18 +0900

    ex5_10 golden test

--- ch05/ex5_10/map_topo_sort_test.go
+++ ch05/ex5_10/map_topo_sort_test.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"io"
-	"net/http"
-	"net/url"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"testing"
@@ -12,45 +10,24 @@ import (
 )
 
 func TestMapTopoSort(t *testing.T) {
-	u, err := url.Parse("https://raw.githubusercontent.com/adonovan/gopl.io/master/ch5/toposort/main.go")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	originalGoCodePath := "original.go"
-
-	defer func() {
-		if err := os.Remove(originalGoCodePath); !os.IsNotExist(err) || err != nil {
-			t.Fatal(err)
-		}
-	}()
-
-	resp, err := http.Get(u.String())
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	out, err := os.Create(originalGoCodePath)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer out.Close()
-
-	_, err = io.Copy(out, resp.Body)
-
 	// run map_topo_sort.go
 	outMapTopo, err := exec.Command("go", "run", "map_topo_sort.go").Output()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	outOrignalTopo, err := exec.Command("go", "run", out.Name()).Output()
+	f, err := os.Open("./test/golden.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+
+	b, err := ioutil.ReadAll(f)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if diff := cmp.Diff(outOrignalTopo, outMapTopo); diff != "" {
+	if diff := cmp.Diff(string(b), string(outMapTopo)); diff != "" {
 		t.Fatalf("invalid output, diff: %v\n", diff)
 	}
 }
