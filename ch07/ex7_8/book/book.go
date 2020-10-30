45624e47403b828add459413cff3bfdf67874631
hatobus <s1240056@gmail.com>
2020-10-30 17:00:15 +0900

    update

--- ch07/ex7_8/book/book.go
+++ ch07/ex7_8/book/book.go
@@ -11,34 +11,34 @@ type Book struct {
 
 func TitleCmp(b1, b2 *Book) int {
 	if b1.Title < b2.Title {
-		return 1
+		return -1
 	}
 	if b1.Title == b2.Title {
 		return 0
 	}
-	return -1
+	return 1
 }
 
 func WriterCmp(b1, b2 *Book) int {
 	if b1.Writer < b2.Writer {
-		return 1
+		return -1
 	}
 	if b1.Writer == b2.Writer {
 		return 0
 	}
-	return -1
+	return 1
 }
 
 func PublishCmp(b1, b2 *Book) int {
 	if b1.PublishAt.Before(b2.PublishAt) {
-		return 1
+		return -1
 	}
 
 	if b1.PublishAt == b2.PublishAt {
 		return 0
 	}
 
-	return -1
+	return 1
 }
 
 func PageCmp(b1, b2 *Book) int {
