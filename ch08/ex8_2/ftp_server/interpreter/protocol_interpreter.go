2da8615b0bb552426a3fb094f7dbf0e2f797ffc9
hatobus <s1240056@gmail.com>
2020-11-22 18:11:13 +0900

    ex8-2 ファイルを分ける

--- ch08/ex8_2/ftp_server/interpreter/protocol_interpreter.go
+++ ch08/ex8_2/ftp_server/interpreter/protocol_interpreter.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"net"
 	"os"
@@ -52,6 +51,7 @@ func (pi *interpreter) setWorkingDir() error {
 
 func (pi *interpreter) checkPathExist(path string) (*fileInfo, error) {
 	dstPath := filepath.Join(pi.wd, path)
+	log.Println(dstPath)
 	if _, err := os.Stat(dstPath); os.IsNotExist(err) {
 		dstPath = path
 	} else if !os.IsNotExist(err) {
@@ -270,150 +270,4 @@ func (pi *interpreter) hostPortFTP(address string) (string, error) {
 	}
 
 	return fmt.Sprintf("%d.%d.%d.%d:%d", h1, h2, h3, h4, 256*p1+p2), nil
-}
-
-func (pi *interpreter) list(dst string) (int, error) {
-	conn, err := pi.dataConnection()
-	if err != nil {
-		return StatusBadCommand, err
-	}
-	defer conn.Close()
-
-	fi := new(fileInfo)
-
-	if fi, err = pi.checkPathExist(dst); os.IsNotExist(err) {
-		pi.printf("%v: No such file or directory ", dst)
-		return StatusBadArguments, nil
-	} else if err != nil {
-		log.Println(err)
-		pi.printf("%v: server error ", dst)
-		return StatusFileUnavailable, nil
-	}
-
-	if fi.info.IsDir() {
-		files, err := ioutil.ReadDir(fi.path)
-		if err != nil {
-			pi.printf("%v: No such file or directory ", dst)
-			return StatusBadArguments, nil
-		}
-
-		pi.printf("%v\r\n", StatusAboutToSend)
-
-		for _, f := range files {
-			_, err := fmt.Fprint(conn, f.Name(), "\n")
-			if err != nil {
-				return StatusBadCommand, err
-			}
-		}
-	} else {
-		pi.printf("%v\r\n", StatusAboutToSend)
-		_, err := fmt.Fprint(conn, fi.info.Name(), "\n")
-		if err != nil {
-			return StatusBadCommand, err
-		}
-	}
-
-	return StatusClosingDataConnection, nil
-}
-
-func (pi *interpreter) delete(dst string) (int, error) {
-	var err error
-	fi := new(fileInfo)
-	if fi, err = pi.checkPathExist(dst); os.IsNotExist(err) {
-		pi.printf("%v: No such file or directory ", dst)
-		return StatusBadArguments, nil
-	} else if err != nil {
-		log.Println(err)
-		pi.printf("%v: server error ", dst)
-		return StatusFileUnavailable, nil
-	}
-
-	if fi.info.IsDir() {
-		if err := os.RemoveAll(fi.path); err != nil {
-			return StatusActionAborted, nil
-		}
-	} else {
-		if err := os.Remove(fi.path); err != nil {
-			return StatusActionAborted, nil
-		}
-	}
-
-	return StatusCommandOK, nil
-}
-
-func (pi *interpreter) retr(dst string) (int, error) {
-	var err error
-	fi := new(fileInfo)
-	if fi, err = pi.checkPathExist(dst); os.IsNotExist(err) {
-		pi.printf("%v: No such file or directory ", dst)
-		return StatusBadArguments, nil
-	} else if err != nil {
-		log.Println(err)
-		pi.printf("%v: server error ", dst)
-		return StatusFileUnavailable, nil
-	}
-
-	var retrFile string
-
-	if fi.info.IsDir() {
-		pi.printf("%v is directory, please input files ", fi.path)
-		return StatusFileUnavailable, nil
-	} else {
-		retrFile = fi.path
-	}
-
-	pi.printf("150 sending. \r\n")
-
-	conn, err := pi.dataConnection()
-	if err != nil {
-		pi.printf("Data connection open failed ")
-		return StatusFileUnavailable, nil
-	}
-
-	err = pi.send(conn, retrFile)
-	if err != nil {
-		log.Println(err)
-		return StatusFileUnavailable, err
-	}
-	conn.Close()
-
-	return StatusClosingDataConnection, nil
-}
-
-func (pi *interpreter) send(conn io.ReadWriteCloser, file string) error {
-	f, err := os.Open(file)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	if pi.binaryOption {
-		_, err = io.Copy(conn, f)
-		if err != nil {
-			log.Println(err)
-			pi.printf("file unavailable ")
-			return fmt.Errorf("io copy failed")
-		}
-	} else {
-		r := bufio.NewReader(f)
-		w := bufio.NewWriter(conn)
-
-		for {
-			l, isPrefix, err := r.ReadLine()
-			if err != nil {
-				if err == io.EOF {
-					break
-				}
-				log.Println(err)
-				pi.printf("file unavailable ")
-				return fmt.Errorf("read line failed")
-			}
-
-			w.Write(l)
-			if !isPrefix {
-				w.Write([]byte("\r\n"))
-			}
-		}
-	}
-	return nil
 }
