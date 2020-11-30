e548eece0bef5a116797e2940ff94816908abfa2
hatobus <s1240056@gmail.com>
2020-11-30 22:49:08 +0900

    ex8_8 echo with time out

---
+++ ch08/ex8_8/abort_echo.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+)
+
+var delay = 1 * time.Second
+var timeOut = 10 * time.Second
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+
+	s := bufio.NewScanner(c)
+	scanLines := make(chan string)
+
+	go func() {
+		for s.Scan() {
+			scanLines <- s.Text()
+		}
+	}()
+
+	for {
+		select {
+		case scanLine, ok := <-scanLines:
+			if !ok {
+				return
+			}
+			go echo(c, scanLine, 1*time.Second)
+		case <-time.After(timeOut):
+			return
+		}
+	}
+}
+
+func main() {
+	l, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
