ed6a52a6fb489ce5e678b67dd195d98638b50f84
hatobus <s1240056@gmail.com>
2021-02-05 01:23:44 +0900

    ex13-4 os.exec bzip writer

--- ch13/ex13_4/bzip.go
+++ ch13/ex13_4/bzip.go
@@ -1,1 +1,47 @@
 package ex13_4
+
+import (
+	"io"
+	"os/exec"
+)
+
+type writer struct {
+	cmd   exec.Cmd
+	stdin io.WriteCloser
+}
+
+func NewWriter(w io.Writer) (io.WriteCloser, error) {
+	cmd := exec.Cmd{
+		Path:   "/bin/bzip2",
+		Stdout: w,
+	}
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return nil, err
+	}
+
+	cmd.Start()
+	if err != nil {
+		return nil, err
+	}
+	return &writer{
+		cmd,
+		stdin,
+	}, nil
+}
+
+func (w *writer) Write(data []byte) (int, error) {
+	return w.stdin.Write(data)
+}
+
+func (w *writer) Close() error {
+	pipelineErr := w.stdin.Close()
+	cmdErr := w.cmd.Wait()
+	if pipelineErr != nil {
+		return pipelineErr
+	}
+	if cmdErr != nil {
+		return cmdErr
+	}
+	return nil
+}
--- ch13/ex13_4/bzip_test.go
+++ ch13/ex13_4/bzip_test.go
@@ -1,1 +1,31 @@
 package ex13_4
+
+import (
+	"bytes"
+	"compress/bzip2"
+	"io"
+	"testing"
+)
+
+func TestBzip(t *testing.T) {
+	var compressed, uncompressed bytes.Buffer
+	w, err := NewWriter(&compressed)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	mw := io.MultiWriter(w, &uncompressed)
+	for i := 0; i < 10000; i++ {
+		io.WriteString(mw, "aaaaa")
+	}
+	if err := w.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	var uc bytes.Buffer
+	io.Copy(&uc, bzip2.NewReader(&compressed))
+	if !bytes.Equal(uncompressed.Bytes(), uc.Bytes()) {
+		t.Error("failed to decompression")
+	}
+}
